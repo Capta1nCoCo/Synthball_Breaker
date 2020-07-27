35dd99eecacc4837bc1b4ebf48e8f76e7bf36569
Capta1nCoCo <femabu28@gmail.com>
2020-07-27 16:51:57 +0300

    65. Count Breakable Blocks

--- Synthball_Breaker/Assets/Scripts/Block.cs
+++ Synthball_Breaker/Assets/Scripts/Block.cs
@@ -6,10 +6,19 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 2f);
         Destroy(gameObject);
+        level.RemoveDestroyedBlocks();
     }
 
 }
---
+++ Synthball_Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks;
+
+    SceneLoader sceneLoader;
+
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        LevelComplete();
+    }
+    
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void RemoveDestroyedBlocks()
+    {
+        breakableBlocks--;
+    }
+
+    private void LevelComplete()
+    {
+        if (breakableBlocks == 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
