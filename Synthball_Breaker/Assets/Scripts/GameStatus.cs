a19913e1eae5a79f4a5142c356516750733f9614
Capta1nCoCo <femabu28@gmail.com>
2020-07-28 11:40:38 +0300

    70. Implement Singleton Pattern & 71. Singleton Pattern Bug Fix

--- Synthball_Breaker/Assets/Scripts/GameStatus.cs
+++ Synthball_Breaker/Assets/Scripts/GameStatus.cs
@@ -7,9 +7,26 @@ public class GameStatus : MonoBehaviour
 {
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 20;
-    [SerializeField] TextMeshProUGUI scoreText;
+    
 
     [SerializeField] int currentScore = 0;
+
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {        
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        
+    }
 
     private void Start()
     {
@@ -22,7 +39,7 @@ public class GameStatus : MonoBehaviour
     }
     
     private void DisplayCurrentScore()
-    {
+    {        
         scoreText.text = currentScore.ToString();
     }
 
